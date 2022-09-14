3f30f7cce8f77b441d0f896b44369a24c0892548
mbolang77 <78078674+mbolang77@users.noreply.github.com>
2022-09-15 01:31:37 +0700

    Update check_amd64.sh

--- check_amd64.sh
+++ check_amd64.sh
@@ -15,11 +15,11 @@ determine_level () {
   level=0
   has_flags lm cmov cx8 fpu fxsr mmx syscall sse2 || return 0
   level=1
-  has_flags cx16 lahf_lm popcnt sse4_1 sse4_2 ssse3 || return 0
+  has_flags cx16 lahf_lm popcnt sse4_1 sse4_2 ssse3 || return 1
   level=2
-  has_flags avx avx2 bmi1 bmi2 f16c fma abm movbe xsave || return 0
+  has_flags avx avx2 bmi1 bmi2 f16c fma abm movbe xsave || return 1
   level=3
-  has_flags avx512f avx512bw avx512cd avx512dq avx512vl || return 0
+  has_flags avx512f avx512bw avx512cd avx512dq avx512vl || return 1
   level=4
 }
 
